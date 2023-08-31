48cfaa4953db008a21904eba1d124a77a6f00c62
akm4545 <akm4545@naver.com>
2023-08-31 17:18:47 +0900

    chapter4

---
+++ chapter2/optimagrowth/src/main/java/com/optimagrowth/optimagrowth/license/controller/LicenceController.java
@@ -0,0 +1,63 @@
+package com.optimagrowth.optimagrowth.license.controller;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+import com.optimagrowth.optimagrowth.license.model.License;
+import com.optimagrowth.optimagrowth.license.service.LicenseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.hateoas.Link;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
+
+@RestController
+@RequestMapping(value = "v1/organization/{organizationId}/license")
+public class LicenceController {
+
+    @Autowired
+    private LicenseService licenseService;
+
+    @GetMapping(value = "/{licenseId}")
+    public ResponseEntity<License> getLicense(@PathVariable("organizationId") String organizationId, @PathVariable("licenseId") String licenseId){
+        License license = licenseService.getLicense(licenseId, organizationId);
+
+        license.add(linkTo(methodOn(LicenceController.class)
+                    .getLicense(organizationId, license.getLicenseId()))
+                .withSelfRel(),
+                //루트 매핑 획득
+                linkTo(
+                        //메서드 매핑 획득
+                        methodOn(LicenceController.class)
+                        .createLicense(organizationId, license, null))
+                .withRel("createLicense"),
+                linkTo(methodOn(LicenceController.class)
+                        .updateLicense(organizationId, license))
+                .withRel("updateLicense"),
+                linkTo(methodOn(LicenceController.class)
+                        .deleteLicense(organizationId, license.getLicenseId()))
+                .withRel("deleteLicense")
+        );
+
+        return ResponseEntity.ok(license);
+    }
+
+    @PutMapping
+    public ResponseEntity<String> updateLicense(@PathVariable("organizationId") String organizationId, @RequestBody License request){
+        return ResponseEntity.ok(licenseService.updateLicense(request, organizationId));
+    }
+
+    @PostMapping
+    public ResponseEntity<String> createLicense(@PathVariable("organizationId") String organizationId,
+                                                @RequestBody License request,
+                                                @RequestHeader(value = "Accept-Language", required = false) Locale locale){
+        return ResponseEntity.ok(licenseService.createLicense(request, organizationId, locale));
+    }
+
+    @DeleteMapping(value = "/{licenseId}")
+    public ResponseEntity<String> deleteLicense(@PathVariable("organizationId") String organizationId, @PathVariable("licenseId")String licenseId){
+        return ResponseEntity.ok(licenseService.deleteLicense(licenseId, organizationId));
+    }
+}
---
+++ chapter2/optimagrowth/src/main/java/com/optimagrowth/optimagrowth/license/model/License.java
@@ -0,0 +1,24 @@
+package com.optimagrowth.optimagrowth.license.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.hateoas.RepresentationModel;
+
+@Getter
+@Setter
+@ToString
+//HATEOAS 사용 다음단계의 api 정보를 제공하기 위한 준비
+public class License extends RepresentationModel<License> {
+    private int id;
+
+    private String licenseId;
+
+    private String description;
+
+    private String organizationId;
+
+    private String productName;
+
+    private String licenseType;
+}
