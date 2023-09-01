c77019e98f3cd55c3e4a9e4b8422074fee93c35f
akm4545 <akm4545@naver.com>
2023-09-01 14:58:57 +0900

    chapter5

---
+++ chapter5/configserver/configserver/src/main/java/com/optmagrowth/configserver/ConfigurationServerApplication.java
@@ -0,0 +1,16 @@
+package com.optmagrowth.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+//스프링 클라우드 컨피그 서비스 활성화
+@EnableConfigServer
+public class ConfigurationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigurationServerApplication.class, args);
+	}
+
+}
---
+++ chapter5/configserver/configserver/src/test/java/com/optmagrowth/configserver/ConfigurationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.optmagrowth.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
