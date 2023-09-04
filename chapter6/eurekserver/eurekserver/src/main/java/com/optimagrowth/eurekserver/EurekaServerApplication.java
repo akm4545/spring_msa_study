0c068e8df6ae0da32da39a24cf597325245fceea
akm4545 <akm4545@naver.com>
2023-09-04 19:17:15 +0900

    chapter6

--- chapter6/eurekserver/eurekserver/src/main/java/com/optimagrowth/eurekserver/EurekaServerApplication.java
+++ chapter6/eurekserver/eurekserver/src/main/java/com/optimagrowth/eurekserver/EurekaServerApplication.java
@@ -2,8 +2,11 @@ package com.optimagrowth.eurekserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+//유레카 서버 활성화
+@EnableEurekaServer
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
