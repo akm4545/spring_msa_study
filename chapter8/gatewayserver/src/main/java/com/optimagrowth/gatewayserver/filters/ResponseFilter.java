3c94549ae390d427352e0eb08b1d77c7d2f2707b
akm4545 <akm4545@naver.com>
2023-10-04 19:54:06 +0900

    chapter11

--- chapter8/gatewayserver/src/main/java/com/optimagrowth/gatewayserver/filters/ResponseFilter.java
+++ chapter8/gatewayserver/src/main/java/com/optimagrowth/gatewayserver/filters/ResponseFilter.java
@@ -1,5 +1,6 @@
 package com.optimagrowth.gatewayserver.filters;
 
+import brave.Tracer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,27 +16,48 @@ import java.util.logging.Filter;
 public class ResponseFilter {
     final Logger logger = LoggerFactory.getLogger(ResponseFilter.class);
 
+//   추적 ID 및 스팬 ID 정보에 접근하는 진입점 설정
+    @Autowired
+    Tracer tracer;
+
     @Autowired
     FilterUtils filterUtils;
+
+//    @Bean
+//    public GlobalFilter postGlobalFilter(){
+//        return (exchange, chain) -> {
+//           return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+//               HttpHeaders requestHeaders = exchange.getRequest().getHeaders();
+////               원본 http 요청에 전달된 상관관계 ID 를 가져온다
+//               String correlationId = filterUtils.getCorrelationId(requestHeaders);
+//
+//               logger.debug("Adding the correlation id to the outbound headers. {}", correlationId);
+//
+//               //응답에 상관관계 id를 삽입한다.
+//               exchange.getResponse().getHeaders().add(FilterUtils.CORRELATION_ID, correlationId);
+//
+////               발신 요청 URI 로깅
+//               logger.debug("Completing outgoing request for {}.", exchange.getRequest().getURI());
+//
+//            }));
+//        };
+//    }
 
     @Bean
     public GlobalFilter postGlobalFilter(){
         return (exchange, chain) -> {
-           return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-               HttpHeaders requestHeaders = exchange.getRequest().getHeaders();
-//               원본 http 요청에 전달된 상관관계 ID 를 가져온다
-               String correlationId = filterUtils.getCorrelationId(requestHeaders);
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                //슬루스 추적 Id의 응답 헤더 tmx-correlation-id 에 스팬 추가
+                String traceId = tracer.currentSpan()
+                        .context()
+                        .traceIdString();
 
-               logger.debug("Adding the correlation id to the outbound headers. {}", correlationId);
+                logger.debug("Adding the correlation id to the outbound headers. {}", traceId);
 
-               //응답에 상관관계 id를 삽입한다.
-               exchange.getResponse().getHeaders().add(FilterUtils.CORRELATION_ID, correlationId);
+                exchange.getResponse().getHeaders().add(FilterUtils.CORRELATION_ID, traceId);
 
-//               발신 요청 URI 로깅
-               logger.debug("Completing outgoing request for {}.", exchange.getRequest().getURI());
-
+                logger.debug("Completing outgoing request for . {}", exchange.getRequest().getURI());
             }));
         };
     }
-
 }
